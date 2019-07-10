9f206558c2ada029907533bc2d050eaff02d3db8
Jennifer Parker <jennifer.parker1@verizon.com>
2019-07-10 13:34:36 -0500

    afterennon cmmit

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,16 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+//Game.js to create a Game class with methods for starting and ending the game, handling interactions, getting a random phrase, checking for a win, and removing a life from the scoreboard.
+ 
+
+class Game{
+
+    constructor() {
+        this.missed = 0;
+        this.phrases = this.createPhrases();
+        this.activePhrase = null;
+
+
+
+ }}
--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+const phrase = new Phrase();
+const game = new Game();
+//app.js to create a new instance of the Game class and 
+
+//add event listeners for the start button and onscreen keyboard buttons.
